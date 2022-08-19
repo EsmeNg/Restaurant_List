aaa12f7950bbb13d45096392e60188b8424ada76
EsmeNg <nwmx3x@gmail.com>
2022-08-19 13:07:08 +0800

    feat: add delete restaurant function

--- app.js
+++ app.js
@@ -95,6 +95,17 @@ app.post('/restaurants/:id/edit', (req,res) => {
   .catch(error => console.error(error))
 })
 
+//  設置路由: 刪除餐廳資料
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+  .then(restaurant => restaurant.remove())
+  .then(() => res.redirect('/'))
+  .catch(error => console.error(error))
+})
+
+
+
 //  設置路由: 用戶搜尋餐龐的結果
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
